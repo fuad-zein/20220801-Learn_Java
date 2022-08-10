bf3fdfb579d174b6f43aee0a871f7b5503286120
fuzi <fzein.1695@gmail.com>
2022-08-10 16:01:58 +0700

    logic prima

--- Logic/Task01/Prima/LimitasiPrima01.java
+++ Logic/Task01/Prima/LimitasiPrima01.java
@@ -3,29 +3,61 @@ package Logic.Task01.Prima;
 import java.util.Scanner;
 
 public class LimitasiPrima01 extends Prima {
-  int n, mod;
-  Scanner nilai = new Scanner(System.in);
+  boolean cek = true;
+  int in;
+  Scanner input = new Scanner(System.in);
 
   @Override
   public void valuePrima() {
     System.out.print("Masukkan Nilai = ");
-    n = nilai.nextInt();
+    in = input.nextInt();
 
     System.out.print("Bilangan Prima = ");
-
-    for (int i = 1; i <= n; i++) {
-      mod = 0;
-      for (int j = 1; j <= i; j++) {
+    int i = 2;
+    while (i <= in) {
+      int j = 2;
+      while (j < i) {
         if (i % j == 0) {
-          mod = mod + 1;
+          cek = false;
+          break;
         }
+        j++;
       }
-      if (mod == 2) {
+      if (cek == true) {
         System.out.print(i + " ");
       }
+      i++;
+      cek = true;
     }
   }
 }
+
+// public class LimitasiPrima01 extends Prima {
+// int n;
+// boolean cek;
+// Scanner nilai = new Scanner(System.in);
+
+// @Override
+// public void valuePrima() {
+// System.out.print("Masukkan Nilai = ");
+// n = nilai.nextInt();
+
+// System.out.print("Bilangan Prima = ");
+
+// for (int i = 2; i < n; i++) {
+// cek = true; // bilangan prima
+// for (int j = 2; j <= (i / 2); j++) {
+// if (i % j == 0) {
+// cek = false;
+// break;
+// }
+// }
+// if (cek) {
+// System.out.print(i + " ");
+// }
+// }
+// }
+// }
 
 /*
  * Silahkan buat sebuah algoritma untuk mencetak bilangan prima dengan limitasi
@@ -38,4 +70,21 @@ public class LimitasiPrima01 extends Prima {
  * Contoh 2
  * input : 14
  * Cetak : 2, 3, 5, 7, 11, 13
+ */
+
+/*
+ * 01. False
+ * i = 4
+ * 
+ * j = 2; j <= 2
+ * j = 2 -> 4%2==0 -> bukan bilangan prima
+ * cek = false
+ * 
+ * 02. True
+ * i = 7
+ * 
+ * j = 2;j <= 3 //bukan 3.5 karena int
+ * j = 2 -> 7%2==1 -> cek = true
+ * j = 3 -> 7%3==1 -> cek = true
+ * cek = true; //bilangan prima
  */
--- Logic/Task01/Prima/LimitasiPrima02.java
+++ Logic/Task01/Prima/LimitasiPrima02.java
@@ -3,7 +3,7 @@ package Logic.Task01.Prima;
 import java.util.Scanner;
 
 public class LimitasiPrima02 extends Prima {
-  int mod, a1, a2;
+  int cek, a1, a2;
   Scanner nilai = new Scanner(System.in);
 
   @Override
@@ -15,15 +15,47 @@ public class LimitasiPrima02 extends Prima {
 
     System.out.print("Bilangan Prima       = ");
     for (int i = a1; i <= a2; i++) {
-      mod = 0;
+      cek = 0;
       for (int j = 1; j <= i; j++) {
         if (i % j == 0) {
-          mod = mod + 1;
+          cek = cek + 1;
         }
       }
-      if (mod == 2) {
+      if (cek == 2) {
         System.out.print(i + " ");
       }
     }
   }
 }
+
+// package Logic.Task01.Prima;
+
+// import java.util.Scanner;
+
+// public class LimitasiPrima02 extends Prima {
+// int a1, a2;
+// boolean cek;
+// Scanner nilai = new Scanner(System.in);
+
+// @Override
+// public void valuePrima() {
+// System.out.print("Masukkan Nilai Awal = ");
+// a1 = nilai.nextInt();
+// System.out.print("Masukkan Nilai Akhir = ");
+// a2 = nilai.nextInt();
+
+// System.out.print("Bilangan Prima = ");
+// for (int i = a1; i < a2; i++) {
+// cek = true;
+// for (int j = 2; j <= (i / 2); j++) {
+// if (i % j == 0) {
+// cek = false;
+// break;
+// }
+// }
+// if (cek) {
+// System.out.print(i + " ");
+// }
+// }
+// }
+// }
